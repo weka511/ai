155befcae4393e56a8bd0efe7e5ee7b778face54
Simon Crase <simon@greenweaves.nz>
2022-10-14 15:17:09 +1300

    #2 Plot average

--- selforg.py
+++ selforg.py
@@ -29,6 +29,7 @@ class Rossler:
                  a = 0.2,
                  b = 0.2,
                  c = 5.7):
+        self.name = 'Rossler'
         self.a = a
         self.b = b
         self.c = c
@@ -43,6 +44,7 @@ class Rossler:
 
 class Lorentz:
     def __init__(self):
+        self.name = 'Lorentz'
         self.sigma = 10.0
         self.rho   = 28.0
         self.b     = 8.0/3.0
@@ -56,51 +58,59 @@ class Lorentz:
 
 class Population:
     def __init__(self,oscillator,
-                 stride     = 3,
                  speed    = ones(4),
                  coupling = 0.2,
                  sigma    = ones(4),
                  rng      = default_rng(42)):
         self.oscillator = oscillator
-        self.stride    = stride
-        self.speed     = speed
-        self.coupling  = coupling
-        self.sigma     = sigma
-        self.rng       = rng
+        self.speed      = speed
+        self.coupling   = coupling
+        self.sigma      = sigma
+        self.rng        = rng
 
     def Velocity(self,t,y):
-
-        Average  = array([mean(y[i::self.stride])  for i in range(self.oscillator.d)])
+        Average  = self.get_average(y)
         Noise    = rng.normal(size=len(y))
         Velocity = zeros_like(y)
-        for j,i in enumerate(range(0,len(y),self.stride)):
-            vv = self.oscillator.Velocity(t,y[i:i+stride])
-            Velocity[i:i+stride] = self.speed[j]*(vv+self.coupling*Average) + self.sigma[j] * Noise[i]
+        for oscillator_id,i in enumerate(range(0,len(y),self.oscillator.d)):
+            OscillatorVelocity              = self.oscillator.Velocity(t,y[i:i+self.oscillator.d])
+            VelocityWithCoupling            = self.speed[oscillator_id]*(OscillatorVelocity+self.coupling*Average)
+            Velocity[i:i+self.oscillator.d] = VelocityWithCoupling + self.sigma[oscillator_id] * Noise[i]
         return Velocity
 
+    def get_average(self,y):
+        return array([mean(y[i::self.oscillator.d])  for i in range(self.oscillator.d)])
+
+
+class OscillatorFactory:
+    Oscillators = {}
+    @classmethod
+    def Register(cls,Oscillators):
+        for oscillator in Oscillators:
+            OscillatorFactory.Oscillators[oscillator.name] = oscillator
+    @classmethod
+    def Create(cls,name):
+        return OscillatorFactory.Oscillators[name]
 
 if __name__ == "__main__":
+    OscillatorFactory.Register([Rossler(),Lorentz()])
     parser   = ArgumentParser(__doc__)
-    parser.add_argument('oscillator',choices=['Lorentz','Rossler'])
-    parser.add_argument('--tFinal', type=float, default=5)
-    parser.add_argument('--Nt', type=int, default=1000)
-    parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--N', type=int, default=16)
+    parser.add_argument('oscillator', choices=OscillatorFactory.Oscillators.keys())
+    parser.add_argument('--tFinal',   type=float, default=5)
+    parser.add_argument('--Nt',       type=int,   default=1000)
+    parser.add_argument('--seed',     type=int,   default=42)
+    parser.add_argument('--N',        type=int,   default=16)
     parser.add_argument('--coupling', type=float, default=2.0)
-    parser.add_argument('--show', default=False, action='store_true')
+    parser.add_argument('--show',                 default=False, action='store_true')
     args = parser.parse_args()
+    oscillator = OscillatorFactory.Create(args.oscillator)
+    t          = linspace(0, args.tFinal, args.Nt)
+    rng        = default_rng(args.seed)
+    sspInit    = rng.normal(0,1,oscillator.d*args.N)
+    speed      = exp(rng.normal(0,2**-3,args.N))
+    sigma      = 0.00*ones(args.N)
 
-    t        = linspace(0, args.tFinal, args.Nt)
-
-    rng      = default_rng(args.seed)
-    sspInit  = rng.normal(0,1,3*args.N)
-    stride    = 3
-    speed    = exp(rng.normal(0,2**-3,args.N))
-
-    sigma    = 0.00*ones(args.N)
-    oscillator = Lorentz() if args.oscillator == 'Lorentz' else Rossler()
     population = Population( oscillator,
-                             stride   = stride,
                              speed    = speed,
                              coupling = args.coupling,
                              sigma    = sigma,
@@ -110,9 +120,10 @@ if __name__ == "__main__":
 
     fig = figure(figsize=(6,6))
     ax  = fig.add_subplot(1,1,1)#,projection='3d')
+    ax.plot(t,mean( y[:,0::oscillator.d],axis=1),linestyle='solid')
     m,n = y.shape
-    for i in range(0,n,3):
-        ax.plot(t,y[:,i])
+    for i in range(0,n,oscillator.d):
+        ax.plot(t,y[:,i],linestyle='dotted')
     ax.set_title(args.oscillator)
         # ax.plot(y[:, i], y[:, i+1], y[:, i+2])
     # ax.set_xlabel('x')
