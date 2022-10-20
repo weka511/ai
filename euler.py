1dec260c083e2b41a5216bbc1beaafc8bc30717c
Simon Crase <simon@greenweaves.nz>
2022-10-21 10:30:17 +1300

    #2 Changed docstring

--- euler.py
+++ euler.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-'''Workhorse Runge-Kutta 4th order'''
+'''Solve ODE using Good Old Fashioned Euler'''
 
 from matplotlib.pyplot import figure, show
 from numpy             import array, linspace, size, zeros
