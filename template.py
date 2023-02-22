f28e6a2688559e89030953db8d42e6893907f3a1
Simon Crase <simon@greenweaves.nz>
2023-02-22 19:13:25 +1300

    #9 preliminary template

---
+++ template.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2023 Simon Crase
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+'''Template for Python code'''
+
+
+# Standard library imports.
+
+from argparse import ArgumentParser
+
+# Related third party imports.
+
+from matplotlib.pyplot import figure, show
+import numpy as np
+
+# Local application/library specific imports.
+
+
+if __name__=='__main__':
+    pass
