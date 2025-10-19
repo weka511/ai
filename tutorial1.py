b2978a56ea00ae1c0bd78b8d0fafa00f772e7179
Simon Crase <simon@greenweaves.nz>
2025-10-20 11:53:53 +1300

    #16 Created Context Manager for plotting

--- tutorial1.py
+++ tutorial1.py
@@ -31,6 +31,30 @@ from matplotlib.pyplot import figure, show
 import seaborn as sns
 from pymdp import utils
 
+class AxisIterator:
+    def __init__(self,figsize=(12,12), n_rows = 3, n_columns = 3,figs='figs',title = ''):
+        self.figsize=figsize
+        self.n_rows = n_rows
+        self.n_columns = n_columns
+        self.seq = 0
+        self.title = title
+        self.figs = figs
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.seq += 1
+        return self.fig.add_subplot(self.n_rows,self.n_columns,self.seq)
+
+    def __enter__(self):
+        self.fig = figure(figsize=self.figsize)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.fig.suptitle(self.title)
+        self.fig.tight_layout()
+        self.fig.savefig(join(self.figs,Path(__file__).stem))
 
 def plot_likelihood(matrix, xlabels = list(range(9)), ylabels = list(range(9)), title_str = 'Likelihood distribution (A)',ax=None):
     '''
@@ -114,89 +138,83 @@ def create_B_matrix( actions = ["UP", "DOWN", "LEFT", "RIGHT", "STAY"]):
 if __name__=='__main__':
     args = parse_args()
     rng = np.random.default_rng(args.seed)
-    fig = figure(figsize=(12,12))
     rc('font',**{'family':'serif','serif':['Palatino']})
     rc('text', usetex=True)
-    n_rows = 3
-    n_columns = 3
 
-    #  create a simple categorical distribution
-    my_categorical = rng.random(size=3)
-    my_categorical = utils.norm_dist(my_categorical)
+    with AxisIterator(figs=args.figs,title = 'Tutorial 1: Active inference from scratch') as axes:
 
-    sampled_outcome = utils.sample(my_categorical)
-    plot_beliefs(my_categorical, title_str = f'Random categorical distribution, sample={sampled_outcome}',
-                 ax = fig.add_subplot(n_rows,n_columns,1))
+        #  create a simple categorical distribution
+        my_categorical = rng.random(size=3)
+        my_categorical = utils.norm_dist(my_categorical)
 
-    # Now let’s move onto conditional categorical distributions or likelihoods,
-    # i.e. how we represent the distribution of one discrete random variable X,
-    # conditioned on the settings of another discrete random variable Y.
-    p_x_given_y = rng.random((3, 4))
-    p_x_given_y = utils.norm_dist(p_x_given_y)
-    print(p_x_given_y[:,0])
-    print(p_x_given_y[:,0].reshape(-1,1))
-    print(f'Integral of P(X|Y=0): {p_x_given_y[:,0].sum()}')
-    p_y = np.array([0.75, 0.25]) # this is already normalized - you don't need to `utils.norm_dist()` it!
+        sampled_outcome = utils.sample(my_categorical)
+        plot_beliefs(my_categorical, title_str = f'Random categorical distribution, sample={sampled_outcome}', ax = next(axes))
 
-    # the columns here are already normalized - you don't need to `utils.norm_dist()` it!
-    p_x_given_y = np.array([[0.6, 0.5],
-                            [0.15, 0.41],
-                            [0.25, 0.09]])
+        # Now let’s move onto conditional categorical distributions or likelihoods,
+        # i.e. how we represent the distribution of one discrete random variable X,
+        # conditioned on the settings of another discrete random variable Y.
+        p_x_given_y = rng.random((3, 4))
+        p_x_given_y = utils.norm_dist(p_x_given_y)
+        print(p_x_given_y[:,0])
+        print(p_x_given_y[:,0].reshape(-1,1))
+        print(f'Integral of P(X|Y=0): {p_x_given_y[:,0].sum()}')
+        p_y = np.array([0.75, 0.25]) # this is already normalized - you don't need to `utils.norm_dist()` it!
 
-    print(p_y.round(3).reshape(-1,1))
-    print(p_x_given_y.round(3))
-    E_x_wrt_y = p_x_given_y.dot(p_y)
-    print(E_x_wrt_y)
-    print(f'Integral: {E_x_wrt_y.sum().round(3)}')
+        # the columns here are already normalized - you don't need to `utils.norm_dist()` it!
+        p_x_given_y = np.array([[0.6, 0.5],
+                                [0.15, 0.41],
+                                [0.25, 0.09]])
 
-    # A simple environment: Grid-world
-    # Create  the grid locations in the form of a list of (Y, X) tuples
-    grid_locations = list(product(range(3), repeat = 2))
-    plot_grid(grid_locations,ax=fig.add_subplot(n_rows,n_columns,2))
+        print(p_y.round(3).reshape(-1,1))
+        print(p_x_given_y.round(3))
+        E_x_wrt_y = p_x_given_y.dot(p_y)
+        print(E_x_wrt_y)
+        print(f'Integral: {E_x_wrt_y.sum().round(3)}')
 
-    n_states = len(grid_locations)
-    n_observations = len(grid_locations)
+        # A simple environment: Grid-world
+        # Create  the grid locations in the form of a list of (Y, X) tuples
+        grid_locations = list(product(range(3), repeat = 2))
+        plot_grid(grid_locations,ax = next(axes))
 
-    # The A matrix or $A=P(o|s)
-    # Create an umambiguous or 'noise-less' mapping between hidden states and observations
-    A = np.eye(n_observations, n_states)
-    plot_likelihood(A, title_str = '$A=P(o|s)$',ax=fig.add_subplot(n_rows,n_columns,3))
-    A_noisy = A.copy()
-    A_noisy[0,0] = 1.0/3.0
-    A_noisy[1,0] = 1.0/3.0
-    A_noisy[3,0] = 1.0/3.0
-    plot_likelihood(A_noisy, title_str = 'Blurred A matrix',ax=fig.add_subplot(n_rows,n_columns,4))
+        n_states = len(grid_locations)
+        n_observations = len(grid_locations)
 
-    my_A_noisy = A_noisy.copy()
+        # The A matrix or $A=P(o|s)
+        # Create an umambiguous or 'noise-less' mapping between hidden states and observations
+        A = np.eye(n_observations, n_states)
+        plot_likelihood(A, title_str = '$A=P(o|s)$',ax = next(axes))
+        A_noisy = A.copy()
+        A_noisy[0,0] = 1.0/3.0
+        A_noisy[1,0] = 1.0/3.0
+        A_noisy[3,0] = 1.0/3.0
+        plot_likelihood(A_noisy, title_str = 'Blurred A matrix',ax = next(axes))
 
-    # locations 3 and 7 are the nearest neighbours to location 6
-    my_A_noisy[3,6] = 1.0 / 3.0
-    my_A_noisy[6,6] = 1.0 / 3.0
-    my_A_noisy[7,6] = 1.0 / 3.0
-    plot_likelihood(my_A_noisy, title_str = 'Noisy A matrix',
-                    ax=fig.add_subplot(n_rows,n_columns,5))
+        my_A_noisy = A_noisy.copy()
 
-    B,actions = create_B_matrix()
+        # locations 3 and 7 are the nearest neighbours to location 6
+        my_A_noisy[3,6] = 1.0 / 3.0
+        my_A_noisy[6,6] = 1.0 / 3.0
+        my_A_noisy[7,6] = 1.0 / 3.0
+        plot_likelihood(my_A_noisy, title_str = 'Noisy A matrix',
+                        ax = next(axes))
 
-    starting_location = (1,0)
-    state_index = grid_locations.index(starting_location)
-    starting_state = utils.onehot(state_index, n_states)
-    plot_point_on_grid(starting_state, grid_locations, ax=fig.add_subplot(n_rows,n_columns,6))
+        B,actions = create_B_matrix()
 
-    plot_beliefs(starting_state, "Categorical distribution over the starting state", ax=fig.add_subplot(n_rows,n_columns,7))
+        starting_location = (1,0)
+        state_index = grid_locations.index(starting_location)
+        starting_state = utils.onehot(state_index, n_states)
+        plot_point_on_grid(starting_state, grid_locations, ax = next(axes))
 
-    right_action_idx = actions.index("RIGHT")
-    next_state = B[:,:, right_action_idx].dot(starting_state) # input the indices to the B matrix
-    plot_point_on_grid(next_state, grid_locations, ax=fig.add_subplot(n_rows,n_columns,8))
+        plot_beliefs(starting_state, "Categorical distribution over the starting state", ax = next(axes))
 
-    prev_state = next_state.copy()
-    down_action_index = actions.index("DOWN")
-    next_state = B[:,:,down_action_index].dot(prev_state)
-    plot_point_on_grid(next_state, grid_locations, ax=fig.add_subplot(n_rows,n_columns,9))
+        right_action_idx = actions.index("RIGHT")
+        next_state = B[:,:, right_action_idx].dot(starting_state) # input the indices to the B matrix
+        plot_point_on_grid(next_state, grid_locations, ax = next(axes))
 
-    fig.suptitle('Tutorial 1: Active inference from scratch')
-    fig.tight_layout()
-    fig.savefig(join(args.figs,Path(__file__).stem))
+        prev_state = next_state.copy()
+        down_action_index = actions.index("DOWN")
+        next_state = B[:,:,down_action_index].dot(prev_state)
+        plot_point_on_grid(next_state, grid_locations,ax = next(axes))
 
     if args.show:
         show()
