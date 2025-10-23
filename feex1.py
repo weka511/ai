61c54d1a6066cf0c5bec3b8c0c9c53341ec88964
Simon Crase <simon@greenweaves.nz>
2025-10-23 19:39:00 +1300

    #10 Documentation

--- feex1.py
+++ feex1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2020-2022 Greenweaves Software Limited
+# Copyright (C) 2020-2025 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,10 +19,9 @@
 '''
    Exercise 1, posterior probabilities, from A tutorial on the free-energy
    framework for modelling perception and learning, by Rafal Bogacz.
-   Write a computer program that computes the posterior proababilities of
-   sizes from 0.01 to 5, and ploats them.
+   Write a computer program that computes the posterior probabilities of
+   sizes from 0.01 to 5, and plots them.
 '''
-
 
 from matplotlib.pyplot import figure, show
 import numpy as np
@@ -30,22 +29,44 @@ from scipy.integrate import quad
 from scipy.stats import norm
 
 def p_u_v(u,v,Sigma_u=1, g=lambda v:v**2):
-    '''Likelihood'''
+    '''
+    Calculate Likelihood
+
+    Parameters:
+        u
+        v
+        Sigma_u
+        g
+    '''
     return norm(g(v),Sigma_u).pdf(u)
 
 def p_v(v, vp=3, Sigma_p=1):
-    '''Prior expectation of size'''
+    '''
+    Prior expectation of size
+
+    Parameters:
+        v
+        vp
+        Sigma_p
+    '''
     return norm(vp, Sigma_p).pdf(v)
 
 def p_u(u):
-    '''Evidence'''
+    '''
+    Calculate Evidence by integrating p_v*p_u_v
+    '''
     return quad(lambda v:p_v(v)*p_u_v(u,v),0,5,epsabs=0.0001)[0]
 
 def get_posterior(v,u):
-    '''Posterior probability'''
+    '''
+    Calculate Posterior probability
+    '''
     return p_v(v)*p_u_v(u,v)/p_u(u)
 
-def get_max_posterior(Posterior):
+def get_max_posterior(Posterior,Sizes):
+    '''
+    Calculate maximum posterior proability
+    '''
     index_max = np.argmax(Posterior)
     return Sizes[index_max],Posterior[index_max]
 
@@ -54,20 +75,13 @@ if __name__ == '__main__':
     Sizes = np.linspace(0,5,num=500)
     evidence = p_u(u)
     Posterior = get_posterior(Sizes,u)
-    x,y = get_max_posterior(Posterior)
-
+    x,y = get_max_posterior(Posterior,Sizes)
     fig = figure(figsize=(10,10))
     ax = fig.add_subplot(1,1,1)
-    ax.scatter(Sizes,Posterior,
-               s     = 1,
-               c     = 'xkcd:blue',
-               label = 'Posterior probability of sizes')
+    ax.scatter(Sizes,Posterior, s = 1, c = 'xkcd:blue', label = 'Posterior probability of sizes')
     ax.set_xlabel('v')
     ax.set_ylabel('p(v|u)')
-    ax.vlines(x,0,y,
-              colors     = 'xkcd:red',
-              linestyles = 'dotted',
-              label      = f'Max posterior={x:.2f}')
+    ax.vlines(x,0,y, colors = 'xkcd:red', linestyles = 'dotted', label = f'Max posterior={x:.2f}')
     ax.legend()
     ax.set_title('Bogacz, Exercise 1')
     fig.savefig('figs/feex1')
