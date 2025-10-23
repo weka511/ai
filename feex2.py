fff2a0676af1034ca81c71d4e59cb9603e4953fb
Simon Crase <simon@greenweaves.nz>
2025-10-24 09:55:04 +1300

    #10 Use np.fromiter to initialize array. Added doco

--- feex2.py
+++ feex2.py
@@ -28,27 +28,25 @@ from matplotlib.pyplot import figure, show
 from matplotlib import rc
 import numpy as np
 
-vp = 3    # Mean of prior for food size
-Sigma_p = 1    # Variance of prior
-Sigma_u = 1    # Variance of sensory noise
-u = 2   # Observed light intensity
-
-
-def generate_phi(phi0, N = 500, g = lambda v:v**2, g_prime = lambda v: 2*v, dt = 0.01):
+def generate_phi(phi0, N = 500, g = lambda v:v**2, g_prime = lambda v: 2*v, dt = 0.01,vp = 3,Sigma_p = 1,Sigma_u=1,u = 2):
     '''
-    Generate successive estimates for size of food item
+    Generate successive estimates for size of food item using gradient descent
 
     Parameters:
         phi0        Initial estimate for size
-        N = 500
-        g = lambda v:v**2
-        g_prime = lambda v: 2*v
-        dt
+        N           Number of estimates
+        g           Model of how intensity depends on size
+        g_prime     Derivative of g
+        dt          Time interval
+        vp          Mean of prior for food size
+        Sigma_p     Variance of prior
+        Sigma_u     Variance of sensory noise
+        u           Observed light intensity
     '''
     phi = phi0
     yield phi
     for i in range(N):
-        df = (vp - phi)/Sigma_p + (u - g(phi))*g_prime(phi)/Sigma_u
+        df = (vp - phi)/Sigma_p + (u - g(phi)) * g_prime(phi)/Sigma_u  # Negative gradient
         phi += dt*df
         yield phi
 
@@ -57,8 +55,13 @@ if __name__ == '__main__':
     T0 = 0
     T1 = 5
     N = 500
+    vp = 3    # Mean of prior for food size
+    Sigma_p = 1    # Variance of prior
+    Sigma_u = 1    # Variance of sensory noise
+    u = 2   # Observed light intensity
     Ts = np.linspace(T0,T1,num=N+1)
-    Phis = np.array(list(generate_phi(vp,N=N,dt=(T1-T0)/N)))
+    Phis = np.fromiter(generate_phi(vp,N=N,dt=(T1-T0)/N,u=u,vp=vp,Sigma_p=Sigma_p,Sigma_u=Sigma_u),
+                       dtype=float)
 
     fig = figure(figsize=(10,10))
     ax  = fig.add_subplot(1,1,1)
