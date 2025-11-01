e423de1187277297e85a2fa2391447846cf81fe7
Simon Crase <simon@greenweaves.nz>
2025-11-01 14:40:47 +1300

    #18 First atteempt to implemenet (4.13)

--- hmm_7_2.py
+++ hmm_7_2.py
@@ -15,10 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-'''HMM Example from Section 7-2'''
+'''
+    Hidden Markov Model Example for musical notes
 
-
-# Standard library imports.
+    Produce simulated inference plots to illustrate belief updating
+    based on the generative model outlined in Section 7-2 Perceptual Processing
+'''
 
 from argparse import ArgumentParser
 from os.path import join
@@ -29,16 +31,19 @@ import numpy as np
 from pymdp.maths import softmax, spm_log_single as log_stable
 import seaborn as sns
 
+
 def parse_args():
     parser = ArgumentParser(__doc__)
     parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
     parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
     return parser.parse_args()
 
+
 class AxisIterator:
     '''
     This class creates subplots as needed
     '''
+
     def __init__(self, figsize=(8, 8), n_rows=2, n_columns=2, figs='figs', title='', show=False, name=Path(__file__).stem):
         self.figsize = figsize
         self.n_rows = n_rows
@@ -65,20 +70,21 @@ class AxisIterator:
 
     def __enter__(self):
         rc('font', **{'family': 'serif',
-                              'serif': ['Palatino'],
-                    'size': 8})
+                      'serif': ['Palatino'],
+                      'size': 8})
         rc('text', usetex=True)
         self.fig = figure(figsize=self.figsize)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.fig.suptitle(self.title, fontsize=10)
-        self.fig.tight_layout(pad=3,h_pad=4,w_pad=3)
+        self.fig.suptitle(self.title, fontsize=12)
+        self.fig.tight_layout(pad=3, h_pad=4, w_pad=3)
         self.fig.savefig(join(self.figs, self.name))
         if self.show:
             show()
 
-def create_P_state_symbol(prior,A,B):
+
+def create_P_state_symbol(prior, A, B):
     '''
     Create a matrix of containing the probability of being in each state and emitting each symbol,
     given the probabilties of being in each state from the prvious step.
@@ -89,48 +95,81 @@ def create_P_state_symbol(prior,A,B):
         B       Transition matrix, each row being the probabilty of one particular state
                 as a transition from previous state
     '''
-    n_symbols,n_states = A.shape
-    product = np.zeros((n_symbols,n_states))
-    P_Trans = np.dot(B,prior) # Probabilities of each state, given prior
+    n_symbols, n_states = A.shape
+    P_state_symbol = np.zeros((n_symbols, n_states))
+    P_Trans = np.dot(B, prior) # Probabilities of each state, given prior
     for i in range(n_symbols):
         for j in range(n_states):
-            product[i,j] = P_Trans[j] * A[i,j]
-    return product
+            P_state_symbol[i, j] = P_Trans[j] * A[i, j]
+    return P_state_symbol
 
-def create_beliefs(A,B,D,o):
+
+def create_beliefs(A, B, D, o):
     '''
     Calculate the Observer's beliefs, i.e. her estimate of the probability
     of being in each hidden state at a particular time, given the observations.
 
     Parameters:
-        A
-        B
-        D
-        o
+        A     Emission matrix, the probability of one particular output given the state.
+        B     Transition matrix, each row being the probabilty of one particular state as a transition from previous state
+        D     Prior probabilities for each state
+        o     The symbols actually observed at each time step
 
     Returns:
         Matrix of probabilties: the rows represent states, and the columns times
     '''
-    n_symbols,n_states = A.shape
+    n_symbols, n_states = A.shape
     n_steps = len(o)
 
-    Beliefs = np.zeros((n_states,n_steps))
+    Beliefs = np.zeros((n_states, n_steps))
 
     tau = 0
-    Beliefs[:,tau] = D
+    Beliefs[:, tau] = D
 
     # Evolve state, based on observed symbols
 
-    for tau in range(1,n_steps):
-        P_state_symbol = create_P_state_symbol(Beliefs[:,tau-1].T,A,B)
-        Beliefs[:,tau] = P_state_symbol[o[tau],:]/np.sum(P_state_symbol[o[tau],:])
+    for tau in range(1, n_steps):
+        P_state_symbol = create_P_state_symbol(Beliefs[:, tau - 1].T, A, B)
+        Beliefs[:, tau] = P_state_symbol[o[tau], :] / np.sum(P_state_symbol[o[tau], :])
 
     return Beliefs
+
+
+def infer(A, B, D, o):
+    '''
+    Infer states using equation 4.13
+
+    Parameters:
+        A     Emission matrix, the probability of one particular output given the state.
+        B     Transition matrix, each row being the probabilty of one particular state as a transition from previous state
+        D     Prior probabilities for each state
+        o     The symbols actually observed at each time step
+    '''
+    n_symbols, n_states = A.shape
+    n_steps = len(o)
+    ln_A = log_stable(A)
+    ln_B = log_stable(B)
+    v = np.zeros((n_states, n_steps))
+    s = np.zeros((n_states, n_steps))
+    s[:, 0] = D
+    o_tau = np.zeros((n_symbols, n_steps))
+    for tau in range(n_steps):
+        o_tau[o[tau], tau] = 1
+
+    tau = 0
+    v[:, 0] = log_stable(s[:, 0])
+    for tau in range(1, n_steps):
+        dv = np.dot(ln_A, o_tau[:, tau - 1]) + 2 * np.dot(ln_B, s[:, tau - 1].T) - log_stable(s[:, tau - 1])
+        v[:, tau] = v[:, tau - 1] + dv
+        s[:, tau] = softmax(v[:, tau])
+
+    return v, s
+
 
 if __name__ == '__main__':
     args = parse_args()
 
-    # Emission matrix, the probabilty of one particular output given the state.
+    # Emission matrix, the probability of one particular output given the state.
     # Each row represents an output, and the coumns represent states
     A = 0.1 * np.array([[7, 1, 1, 1],
                         [1, 7, 1, 1],
@@ -148,29 +187,41 @@ if __name__ == '__main__':
 
     D = np.array([1, 0, 0, 0])
 
-    # Observed symbols
+    # The symbols actually observed at each time step
 
     o = np.array([0, 1, 1, 3, 0])
+
+    v, s = infer(A, B, D, o)
+    n_states, n_steps = s.shape
 
     with AxisIterator(figs=args.figs, title='Figure 7.2: Perceptual Processing',
                       show=args.show, name=Path(__file__).stem) as axes:
 
         ax = next(axes)
-        ax.text(0.4,0.5,'Beliefs: TBP',size='x-large')
+        for i in range(n_states):
+            ax.plot(s[i, :], label=f'{i}')
+        ax.legend(title='State')
+        ax.set_title('Beliefs')
+        ax.set_xlabel(r'$\tau$')
 
         ax = next(axes)
-        sns.heatmap(create_beliefs(A,B,D,o), annot=True, fmt=".1g", ax=ax,cmap='PuRd')
+        sns.heatmap(create_beliefs(A, B, D, o), annot=True, fmt=".1g", ax=ax, cmap='PuRd')
         ax.set_title('Retrospective Beliefs based on observed symbols')
         ax.set_xlabel(r'$\tau$')
         ax.set_ylabel('State')
 
         ax = next(axes)
-        ax.text(0.25,0.5,'Free Energy Gradients: TBP',size='x-large')
+        for i in range(4):
+            ax.plot(v[i, :], label=f'{i}')
+        ax.legend(title='State')
+        ax.set_title('Free Energy Gradients')
+        ax.set_xlabel(r'$\tau$')
 
         ax = next(axes)
-        ax.scatter(list(range(len(o))),o)
+        ax.scatter(list(range(len(o))), o)
         ax.set_xlabel(r'$\tau$')
-        ax.set_xticks(np.arange(0,len(o)))
+        ax.set_xticks(np.arange(0, len(o)))
         ax.set_title('Observed symbols')
-        ax.set_yticks([0,1,2,3])
+        ax.set_yticks([0, 1, 2, 3])
         ax.set_ylabel('Symbol')
+
