af83387f9f26b3a2efa4e804773abdeb963012fb
Simon Crase <simon@greenweaves.nz>
2025-11-05 20:09:08 +1300

    #18 Started on main loop

--- maze_7_3.py
+++ maze_7_3.py
@@ -32,23 +32,20 @@ from pymdp.maths import softmax, spm_log_single as log_stable, spm_norm as norm
 from ai import AxisIterator
 
 
-
 class MDP_Factory:
     '''
     This class creates the A, B, C, and D matrices for the maze example
     '''
-
     def __init__(self):
-        self.context_names = ['Right-Attractive', 'Left-Attractive']
-        self.choice_names = ['Start', 'Hint', 'Left Arm', 'Right Arm']
-        self.choice_action_names = ['Start', 'Hint', 'Left Arm', 'Right Arm']
-        self.state_names = ['Start', 'Hint-Left', 'Hint_Right', 'Left Arm', 'Right Arm']
-        self.obs_names = ['Start', 'Hint-Left', 'Hint_Right', 'Left Arm', 'Right Arm']
+        self.context_names = ['Right Attractive', 'Left Attractive']
+        self.location_names = ['Start', 'Bottom', 'Left', 'Right']
+        self.choice_action_names = ['Move Start', 'Move Bottom', 'Move Left', 'Move Right']
+        self.location_obs_names = ['At Start', 'At Bottom: Left Attractive', 'At Bottom: Right Attractive', 'At Left', 'At Right']
         self.modalities = ['where', 'what']
 
-    def create_A(self, epsilon=2.0 / 100.0):
+    def create_A(self, probability_hint_wrong=2.0 / 100.0):
         A = utils.obj_array(len(self.modalities))
-        A[0] = np.empty((len(self.obs_names), len(self.context_names), len(self.choice_names)))
+        A[0] = np.empty((len(self.location_obs_names), len(self.context_names), len(self.location_names)))
         A[0][:, 0, :] = np.array([[1.0, 0.0, 0.0, 0.0],
                                   [0.0, 0.0, 0.0, 0.0],
                                   [0.0, 1.0, 0.0, 0.0],
@@ -59,18 +56,18 @@ class MDP_Factory:
                                   [0.0, 0.0, 0.0, 0.0],
                                   [0.0, 0.0, 1.0, 0.0],
                                   [0.0, 0.0, 0.0, 1.0]])
-        A[1] = np.empty((3, len(self.context_names), len(self.choice_names)))
+        A[1] = np.empty((3, len(self.context_names), len(self.location_names)))
         A[1][:, 0, :] = np.array([[1.0, 1.0, 0.0, 0.0],
-                                  [0.0, 0.0, epsilon, 1.0 - epsilon],
-                                  [0.0, 0.0, 1.0 - epsilon, epsilon]])
+                                  [0.0, 0.0, probability_hint_wrong, 1.0 - probability_hint_wrong],
+                                  [0.0, 0.0, 1.0 - probability_hint_wrong, probability_hint_wrong]])
         A[1][:, 1, :] = np.array([[1.0, 1.0, 0.0, 0.0],
-                                  [0.0, 0.0, 1.0 - epsilon, epsilon],
-                                  [0.0, 0.0, epsilon, 1.0 - epsilon]])
+                                  [0.0, 0.0, 1.0 - probability_hint_wrong, probability_hint_wrong],
+                                  [0.0, 0.0, probability_hint_wrong, 1.0 - probability_hint_wrong]])
         return A
 
     def create_B(self):
         B = utils.obj_array(len(self.modalities))
-        B[0] = np.zeros((len(self.choice_names), len(self.choice_names), len(self.choice_action_names)))
+        B[0] = np.zeros((len(self.location_names), len(self.location_names), len(self.choice_action_names)))
         B[0][:, :, 0] = np.array([[1.0, 1.0, 0.0, 0.0],
                                   [0.0, 0.0, 0.0, 0.0],
                                   [0.0, 0.0, 1.0, 0.0],
@@ -88,10 +85,10 @@ class MDP_Factory:
                                   [0.0, 0.0, 1.0, 0.0],
                                   [1.0, 1.0, 0.0, 1.0]])
         B[1] = np.zeros((len(self.context_names), len(self.context_names), len(self.choice_action_names)))
-        B[1][:,:,0] = np.eye((len(self.context_names)))
-        B[1][:,:,1] = np.eye((len(self.context_names)))
-        B[1][:,:,2] = np.eye((len(self.context_names)))
-        B[1][:,:,3] = np.eye((len(self.context_names)))
+        B[1][:, :, 0] = np.eye((len(self.context_names)))
+        B[1][:, :, 1] = np.eye((len(self.context_names)))
+        B[1][:, :, 2] = np.eye((len(self.context_names)))
+        B[1][:, :, 3] = np.eye((len(self.context_names)))
         return B
 
     def create_C(self):
@@ -106,24 +103,83 @@ class MDP_Factory:
         D[1] = norm(np.c_[[1.0, 1.0]])
         return D
 
-class Maze(Env):
-    def __init__(self):
-        pass
+
+class MazeEnvironment(Env):
+    '''
+    The class represents the generative process for the maze
+    '''
+
+    def __init__(self, factory, rng=np.random.default_rng(),probability_hint_wrong=2.0 / 100.0):
+        '''
+        Assign context (left or right) at random, and place mouse at starting position.
+
+        Create a table of allowable transitions by or-ing the matrices from Figure 7.6,
+        which  makes Left and Right absorbing states.
+        '''
+        self.rng = rng
+        self.location = 0
+        self.context = self.rng.choice(len(factory.context_names))
+        self.AllowableTransitions = np.array([[1, 1, 0, 0],
+                                              [1, 1, 0, 0],
+                                              [1, 1, 1, 0],
+                                              [1, 1, 0, 1]],dtype=bool)
+        self.probability_hint_wrong = probability_hint_wrong
+
+    def step(self, action):
+        '''
+        Update to position in response to an action
+        '''
+        if self.AllowableTransitions[self.location,action]:
+            self.location = action
+
+        match self.location: #  ['Start', 'Bottom', 'Left', 'Right']
+            case 0:      # Start
+                return 0,0 # At Start
+            case 1:      # Bottom
+                if self.context == 0: # Right Attractive
+                    if self.rng.uniform() < self.probability_hint_wrong:
+                        return 1,0 # At Bottom: Left Attractive
+                    else:
+                        return 2,0 #  At Bottom: Right Attractive
+                else:                 #  Left Attractive
+                    if self.rng.uniform() < self.probability_hint_wrong:
+                        return 2,0 #  At Bottom: Right Attractive
+                    else:
+                        return 1,1 # At Bottom: Left Attractive
+            case 2: # Left
+                if self.context == 0: # Right Attractive
+                    return 3,2   # At Left
+                else:
+                    return 3,1
+            case 3: # Right
+                if self.context == 0: # Right Attractive
+                    return 4,1   # At Left
+                else:
+                    return 4,2
+
 
 def parse_args():
     parser = ArgumentParser(__doc__)
     parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
     parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
+    parser.add_argument('--seed', default=None, type=int, help='Initialize random number generator')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
+    rng = np.random.default_rng(args.seed)
     factory = MDP_Factory()
     agent = Agent(A=factory.create_A(), B=factory.create_B(), C=factory.create_C(), D=factory.create_D())
-    env = Env()
+    env = MazeEnvironment(factory, rng=rng)
+    T = 2
+    action = 0
+    for t in range(T):
+        o = env.step(action)
+        qs = agent.infer_states(o)
 
-    with AxisIterator(figs=args.figs, title='Section 7.3: Decision Making and Planning as Inference',
-                      show=args.show, name=Path(__file__).stem) as axes:
-        pass
+
+    # with AxisIterator(figs=args.figs, title='Section 7.3: Decision Making and Planning as Inference',
+                      # show=args.show, name=Path(__file__).stem) as axes:
+        # pass
 
