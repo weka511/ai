9eee611572651065ce118f97040cd07f8db29888
Simon Crase <simon@greenweaves.nz>
2026-01-30 11:57:43 +1300

    Closed #27

--- establish_subset.py
+++ establish_subset.py
@@ -17,57 +17,50 @@
 
 '''
     Extract subsets of MNIST to facilitate replication
+
+    This program generates a list of indices for data points ensuring that there are
+    precisely n points from each class.
 '''
 
 from argparse import ArgumentParser
-from os.path import splitext, join
+from os.path import join
 from pathlib import Path
 from time import time
-from matplotlib.pyplot import figure, show
-from matplotlib import rc
 import numpy as np
 from mnist import MnistDataloader
 
 
 def parse_args():
     parser = ArgumentParser(__doc__)
-    parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
-    parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
     parser.add_argument('--data', default='./data', help='Location for storing data files')
     parser.add_argument('--out', default=Path(__file__).stem, help='Location for storing indices')
     parser.add_argument('--n', default=1000, type=int, help='Number of images for each class')
     parser.add_argument('--seed', default=None, type=int, help='Seed for random number generator')
     return parser.parse_args()
 
-
-if __name__ == '__main__':
-    rc('font', **{'family': 'serif',
-                  'serif': ['Palatino'],
-                  'size': 8})
-    rc('text', usetex=True)
-    fig = figure(figsize=(24, 12))
-    start = time()
-    args = parse_args()
-    rng = np.random.default_rng(args.seed)
-    nclasses = 10
-    mnist_dataloader = MnistDataloader.create(data=args.data)
-    (_, ytrain), _ = mnist_dataloader.load_data()
-    classes = np.zeros((args.n, nclasses), dtype=int)
+def extract( ytrain, rng = np.random.default_rng(),nclasses = 10,n=1000):
+    '''
+    Create list of indices
+    '''
+    classes = np.zeros((n, nclasses), dtype=int)
     class_counts = np.zeros((nclasses), dtype=int)
     for k in rng.permutation(len(ytrain)):
         image_class = ytrain[k]
         i = class_counts[image_class]
-        if i < args.n:
+        if i < n:
             classes[i, image_class] = k
             class_counts[image_class] += 1
         else:
-            if np.min(class_counts) == args.n:
-                break
+            if np.min(class_counts) == n: return classes
+
+if __name__ == '__main__':
+    start = time()
+    args = parse_args()
+    mnist_dataloader = MnistDataloader.create(data=args.data)
+    (_, ytrain), _ = mnist_dataloader.load_data()
+    classes=extract( ytrain, rng=np.random.default_rng(args.seed),n=args.n)
     np.save(join(args.data, args.out), np.reshape(classes,-1))
     elapsed = time() - start
     minutes = int(elapsed / 60)
     seconds = elapsed - 60 * minutes
     print(f'Elapsed Time {minutes} m {seconds:.2f} s')
-
-    if args.show:
-        show()
