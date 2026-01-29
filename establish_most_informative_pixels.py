7aa0be771a63c4cd97a628e6450d4e1582c9f09b
Simon Crase <simon@greenweaves.nz>
2026-01-30 07:44:38 +1300

    #26 Created code skeleton

---
+++ establish_most_informative_pixels.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2026 Simon Crase  simon@greenweaves.nz
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+'''establish_most_informative_pixels'''
+
+
+from argparse import ArgumentParser
+from pathlib import Path
+import numpy as np
+from pymdp.maths import softmax, spm_log_single as log_stable
+from ai import AxisIterator
+from mnist import MnistDataloader
+
+def parse_args():
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
+    parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
+    parser.add_argument('--data', default='./data', help='Location for storing data files')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    mnist_dataloader = MnistDataloader.create(data=args.data)
+
