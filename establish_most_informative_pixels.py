3f207d4f68517daa48dd830dbed05db51f24af7e
Simon Crase <simon@greenweaves.nz>
2026-01-30 10:21:23 +1300

    #26 Calculate and plot entropies

--- establish_most_informative_pixels.py
+++ establish_most_informative_pixels.py
@@ -15,14 +15,22 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-'''establish_most_informative_pixels'''
+'''
+    Determine which pixels are most relevant to classifying images
+'''
 
 
 from argparse import ArgumentParser
+from os.path import splitext,join
 from pathlib import Path
+from time import time
+from matplotlib.pyplot import figure, show
+from matplotlib import rc
 import numpy as np
+from scipy.stats import entropy
 from pymdp.maths import softmax, spm_log_single as log_stable
-from ai import AxisIterator
+from skimage.exposure import equalize_hist
+from skimage.transform import resize
 from mnist import MnistDataloader
 
 def parse_args():
@@ -32,8 +40,44 @@ def parse_args():
     parser.add_argument('--data', default='./data', help='Location for storing data files')
     return parser.parse_args()
 
+def create_entropies(x_train,selector,bins=11,m=32):
+    n = len(selector)
+    m2 = 32*32
+    images1d = np.zeros((n,m2))
+    for i in selector:
+        img = equalize_hist(resize(np.array(x_train[i]),(32,32)))
+        images1d[i] = np.reshape(img,-1)
+    product = np.zeros((m2))
+    for i in range((m2)):
+        hist,edges = np.histogram(images1d[i],bins=bins,density=True)
+        pdf = hist/np.sum(hist)
+        product[i] = entropy(pdf)
+    return product
+
+
 if __name__ == '__main__':
+    rc('font', **{'family': 'serif',
+                  'serif': ['Palatino'],
+                  'size': 8})
+    rc('text', usetex=True)
+    fig = figure(figsize=(24, 12))
+    start = time()
     args = parse_args()
 
     mnist_dataloader = MnistDataloader.create(data=args.data)
+    (x_train, _), _ = mnist_dataloader.load_data()
+    entropies = create_entropies(x_train,list(range(len(x_train))))
+    ax = fig.add_subplot(1,1,1)
+    fig.colorbar(
+        ax.imshow(np.reshape(entropies,(32,32)),
+                  cmap='viridis'),
+        label='Entropy')
 
+    fig.savefig(join(args.figs,Path(__file__).stem))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
