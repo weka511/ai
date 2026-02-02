c3f508cfd5ad5da10e38d41440a42ba1b80c264b
Simon Crase <simon@greenweaves.nz>
2026-02-02 13:31:31 +1300

    #31 Created skeleton

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ File|Description
 cluster.py|Cluster Analysis for MNIST: establish variability of mutual information within and between classes
 eda.py|Exploratory Data Analysis for MNIST
 eda_mi.py|Exploratory Data Analysis for MNIST: figure out variability of mutual information within and between classes
+establish_styles.py|Establish styles within classes using mutual information
 establish_subset.py|Extract subsets of MNIST to facilitate replication
 establish_most_informative_pixels.py|Determine which pixels are most relevant to classifying images
 mnist.py|Functions for accessing MNIST data
---
+++ establish_styles.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2026 Simon Crase  simon@greenweaves.nz
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+    Establish styles within classes using mutual information
+'''
+from argparse import ArgumentParser
+from os.path import join
+from pathlib import Path
+from time import time
+from matplotlib.pyplot import figure, show
+from matplotlib import rc, cm
+import numpy as np
+from seaborn import lineplot
+from sklearn.feature_selection import mutual_info_classif
+from mnist import MnistDataloader, create_mask,columnize
+
+def parse_args():
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
+    parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
+    parser.add_argument('--data', default='./data', help='Location for storing data files')
+    parser.add_argument('--indices', default='establish_subset.npy', help='Location for storing data files')
+    parser.add_argument('--npairs', default=128, type=int, help='Number of pairs for each class')
+    parser.add_argument('--mask',default=None,help='Name of mask file (omit for no mask)')
+    parser.add_argument('--size', default=28, type=int, help='Number of row/cols in each image: shape will be will be mxm')
+    parser.add_argument('--classes',default=list(range(10)),type=int,nargs='+',help='List of digit classes')
+    parser.add_argument('--bins', default=12, type=int, help='Number of bins for histograms')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    rc('font', **{'family': 'serif',
+                  'serif': ['Palatino'],
+                  'size': 8})
+    rc('text', usetex=True)
+    start = time()
+    args = parse_args()
+    rng = np.random.default_rng()
+    fig = figure(figsize=(8, 8))
+    indices = np.load(join(args.data,args.indices)).astype(int)
+    n_examples,n_classes = indices.shape
+
+    mnist_dataloader = MnistDataloader.create(data=args.data)
+    (x_train, _), _ = mnist_dataloader.load_data()
+    x = columnize(x_train)
+
+    mask,mask_text = create_mask(mask_file=args.mask,data=args.data,size=args.size)
+    mask = mask.reshape(-1)
+    x = np.multiply(x,mask)
+    m,n = indices.shape  # images,classes
+    npairs = min(m,args.npairs)
+    bins = np.linspace(0,1,num=args.bins+1)
+    assert n == 10
+    ax = fig.add_subplot(1,1,1)
+    for i_class in args.classes:
+        print (f'Class {i_class}')
+
+    # fig.savefig(join(args.figs,Path(__file__).stem))
+
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
