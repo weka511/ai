c1aa32780f980e8fd264f88d6dafc5ed75591f9e
Simon Crase <simon@greenweaves.nz>
2026-02-27 10:19:48 +1300

    #58 Added debugging code

--- mnist/build.sh
+++ mnist/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 ./pipeline.py establish-subsets --nimages $1 -o subsets$1
 ./pipeline.py establish-mask --indices subsets$1 -o mask$1
 ./pipeline.py establish-styles --indices subsets$1 --mask mask$1 --o styles$1
