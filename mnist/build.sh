303907e32901cb168bc763cfa293149ee6c2be96
Simon Crase <simon@greenweaves.nz>
2026-03-25 15:51:12 +1300

    #29 As used for testing Gibbs

--- mnist/build.sh
+++ mnist/build.sh
@@ -23,7 +23,8 @@ fi
 if [ $# -gt 0 ];  then
 	./pipeline.py establish-subsets --nimages $1 -o subsets$1 
 	./pipeline.py establish-mask --indices subsets$1 -o mask$1
-	./pipeline.py establish-styles --indices subsets$1 --mask mask$1 --o styles$1 --threshold $THRESHOLD
+	# ./pipeline.py establish-styles --indices subsets$1 --mask mask$1 --o styles$1 --threshold $THRESHOLD
+	./pipeline.py gibbs --indices subsets$1 --mask mask$1 --o styles$1 --M 500
 	./pipeline.py establish-likelihoods --indices subsets$1 --mask mask$1 --styles styles$1 -o likelihoods$1
 	if [ $# -gt 1 ];  then
 		./pipeline.py recognize-digits --indices subsets$1 --mask mask$1 --styles styles$1 --likelihoods likelihoods$1 -o results$1 --N $2
