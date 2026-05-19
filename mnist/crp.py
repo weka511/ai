9a7280553b3282d059ab4261f8b2e60773461b9e
Simon Crase <simon@greenweaves.nz>
2026-05-19 18:19:28 +1200

    #81 Coded minimal build() and gibbs() for Chinese Restaurant Process

--- mnist/crp.py
+++ mnist/crp.py
@@ -18,13 +18,65 @@
 '''
     This module implements the Chinese Restaurant Process
 '''
+from abc import ABC,abstractmethod
+import numpy as np
 
-class ChineseRestaurantProcess:
-    def __init__(self):
-        pass
+class DecayFunction(ABC):
+    '''
+    This class is used to model the decay function of Blei and Frazier
+    '''
+    @abstractmethod
+    def __call__(self,mutual_information):
+        '''
+        Apply decay function
+        '''
+        ...
+
+class NoDecay(DecayFunction):
+    '''
+    Pass mutual information through unchanged
+    '''
+    def __call__(self,mutual_information):
+        return mutual_information    
     
+    
+class ChineseRestaurantProcess:
+    
+    UNASSIGNED = -1
+    
+    def __init__(self,mutual_information,
+                 f=NoDecay(),rng=np.random.default_rng(),alpha=2.0):
+        self.fd = f(1/mutual_information)
+        self.rng = rng
+        self.m,self.n = mutual_information.shape
+        assert self.m == self.n
+        self.rng = rng
+        self.alpha = alpha
+        self.links = np.full((self.m),ChineseRestaurantProcess.UNASSIGNED, dtype=int)
+        
     def build(self):
-        pass
+        indices = self.rng.permutation(self.m)
+        for i in range(self.m):
+            self.links[indices[i]] = self.rng.choice(indices[:i+1],p=self._get_p_init(i))
     
     def gibbs(self):
-        pass
+        for i in range(self.m):
+            self.links[i] = self.rng.choice(self.m,p=self._get_p(i))
+    
+    def _get_p_init(self,i):
+        p = 1/self.fd[i,:]
+        p[i] = self.alpha
+        p = p[:i+1]
+        return p / p.sum()
+            
+    def _get_p(self,i):
+        p = 1/self.fd[i,:]
+        p[i] = self.alpha
+        return p/p.sum() 
+    
+def main():
+    crp = ChineseRestaurantProcess(np.ones((12,12)))
+    crp.build()
+
+if __name__ == '__main__':
+    main()
